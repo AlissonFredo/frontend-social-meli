0640bf02af90b2e3c945dfd7327eafc95ee22437
AlissonFredo <douglasalissonsf@gmail.com>
2026-01-13 16:14:08 -0300

    feat: agrega un diseño estándar con un encabezado al sistema.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
 import './App.css'
+import Layout from './Layout'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/users/:userId/followers" element={<h1>Quem me segue</h1>} />
-        <Route path="/users/:userId/followed" element={<h1>Quem eu sigo</h1>} />
-        <Route path="/users/:userId/feed" element={<h1>Feed de publicações</h1>} />
-        <Route path="/publish" element={<h1>Criar publicação</h1>} />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<h1>Home</h1>} />
+          <Route path="users/:userId/followers" element={<h1>Quem me segue</h1>} />
+          <Route path="users/:userId/followed" element={<h1>Quem eu sigo</h1>} />
+          <Route path="users/:userId/feed" element={<h1>Feed de publicações</h1>} />
+          <Route path="publish" element={<h1>Criar publicação</h1>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
---
+++ src/Layout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router";
+import Header from "./components/Header";
+
+export default function Layout() {
+    return (
+        <div className="min-h-screen bg-meli-gray">
+            <Header />
+
+            <main className="max-w-7xl mx-auto px-4 py-8">
+                <Outlet />
+            </main>
+        </div>
+    )
+}
